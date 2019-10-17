2797fcd09c2bbcf239965541b3e93f9c3582856e
Tayler Ramsay <tramsay@versatilets.com>
2019-10-17 11:59:39 -0400

    test

--- src/components/VideoHTML.js
+++ src/components/VideoHTML.js
@@ -2,7 +2,7 @@ import React from "react"
 import HighPointVideo from "../assets/hightpointdraft.mp4"
 
 export default () => (
-  <video controls autoPlay muted>
+  <video autoPlay muted>
     <source src={HighPointVideo} type="video/mp4" />
   </video>
 )
